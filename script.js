eae45539e82b0d4504887b3f242dac2566a2a3dc
sr5392 <61883265+sr5392@users.noreply.github.com>
2024-01-28 17:53:06 +0100

    Add elements, styling and basic logic

---
+++ script.js
@@ -0,0 +1,17 @@
+let game = (function() {
+
+  const board = document.querySelector(".board");
+  const boxes = document.querySelectorAll(".box");
+  
+  //Bind events
+  boxes.forEach(box => {
+    
+  });
+
+  
+  function render() {
+
+  }
+
+  return 
+})();
